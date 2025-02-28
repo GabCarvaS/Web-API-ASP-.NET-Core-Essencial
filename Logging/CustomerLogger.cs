477bbd5c6524414c9ed63269048dd986e3c3809d
Gabriel Carvalho <gabrielcfacul@gmail.com>
2025-02-28 18:57:13 -0300

    feat(): Log personalizado

---
+++ Logging/CustomerLogger.cs
@@ -0,0 +1,49 @@
+﻿
+namespace APICatalogo.Logging
+{
+    public class CustomerLogger : ILogger
+    {
+        readonly string loggerName;
+        readonly CustomLoggerProviderConfiguration loggerConfig;
+
+        public CustomerLogger(string loggerName, CustomLoggerProviderConfiguration loggerConfig)
+        {
+            this.loggerName = loggerName;
+            this.loggerConfig = loggerConfig;
+        }
+
+        // Not used
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel == loggerConfig.LogLevel;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            string message = $"{logLevel.ToString()} : {eventId.Id} - {formatter(state, exception)}";
+            WriteText(message);
+        }
+
+        private void WriteText(string message)
+        {
+            string logPath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "log.txt");
+            using (StreamWriter streamWriter = new StreamWriter(logPath, true))
+            {
+                try
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
